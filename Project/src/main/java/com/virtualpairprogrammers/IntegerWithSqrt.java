81811b89af6a9b5bd8425907f9c7b153fe415993
Cathy Qin <qin.xuj@northeastern.edu>
2024-10-05 01:32:10 -0700

    pairRdd & reduceByKey

---
+++ Project/src/main/java/com/virtualpairprogrammers/IntegerWithSqrt.java
@@ -0,0 +1,11 @@
+package com.virtualpairprogrammers;
+
+public class IntegerWithSqrt {
+  private int number;
+  private double sqrt;
+
+  public IntegerWithSqrt(int number) {
+    this.number = number;
+    this.sqrt = Math.sqrt(number);
+  }
+}
--- Project/src/main/java/com/virtualpairprogrammers/Main.java
+++ Project/src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,22 +1,25 @@
 package com.virtualpairprogrammers;
 
+import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    List<Double> inputData = new ArrayList<>();
-    inputData.add(35.5);
-    inputData.add(2305.5);
-    inputData.add(9879.5);
-    inputData.add(1.5);
+    List<String> inputData = new ArrayList<>();
+    inputData.add("WARN: Tuesday 4 September 0405");
+    inputData.add("ERROR: Tuesday 4 September 6421");
+    inputData.add("FATAL: Wednesday 4 September 4942");
+    inputData.add("WARN: Saturday 4 September 2038");
 
     // Create a logger
     Logger.getLogger("org.apache").setLevel(Level.WARN);
@@ -24,9 +27,32 @@ public class Main {
     SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
-    JavaRDD<Double> myRdd = sc.parallelize(inputData); // loading a collection and turning it into an RDD
-    Double ret = myRdd.reduce((value1, value2) -> value1 + value2);
-    System.out.println(ret);
+//    JavaRDD<String> originalLogMessages = sc.parallelize(inputData); // loading a collection and turning it into an RDD
+//    JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L));
+//    JavaPairRDD<String, Long> sumsRdd = pairRdd.reduceByKey((value1, value2) -> (value1 + value2));
+//    sumsRdd.foreach(tuple -> System.out.println(tuple._1() + " has " + tuple._2() + " instances."));
+    sc.parallelize(inputData)
+        .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+            .reduceByKey((value1, value2) -> (value1 + value2))
+                .foreach(tuple2 -> System.out.println(tuple2._1 + "has " + tuple2._2 + " instances."));
+
+// groupby key
+//    sc.parallelize(inputData)
+//        .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+//        .groupByKey()
+//            .foreach(tuple -> System.out.println(tuple._1 + "has " + Iterables.size(tuple._2) + " instances."));
+
+//    JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map(value -> new Tuple2<>(value, Math.sqrt(value)));
+//    JavaRDD<Double> sqrtRdd = myRdd.map(value -> Math.sqrt(value));
+//    sqrtRdd.foreach(System.out::println);
+//    sqrtRdd.collect().forEach(System.out::println);
+//    System.out.println(ret);
+
+//     how many elements in sqrtRdd
+//     using just map and reduce
+//    JavaRDD<Long> singleIntegerRdd = sqrtRdd.map(value -> 1L);
+//    Long count = singleIntegerRdd.reduce((value1, value2) -> value1 + value2);
+//    System.out.println(count);
     sc.close();
   }
 }
